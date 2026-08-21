b72515d3d5bde95cae548fa178c8a6d4570c1ed4
z56nnzv559-cell <z56nnzv559@privaterelay.appleid.com>
2026-08-21 16:45:43 +0900

    Mount reviewed marketplace panels

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -5,6 +5,7 @@ import DemoKycHelper from "./DemoKycHelper.jsx";
 import FallbackInbox from "./FallbackInbox.jsx";
 import ProfileEditor from "./ProfileEditor.jsx";
 import JobFormFix from "./JobFormFix.jsx";
+import MarketplaceReview from "./MarketplaceReview.jsx";
 import "./styles.css";
 
 /* 受信側が無いとWeb Pushが成功しても画面には何も出ない。
@@ -23,6 +24,7 @@ createRoot(document.getElementById("root")!).render(
     <App />
     <ProfileEditor />
     <JobFormFix />
+    <MarketplaceReview />
     <DemoKycHelper />
   </React.StrictMode>
 );
